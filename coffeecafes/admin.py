ab04a54bac0f1873bf9f664e92e8b2a56062576e
youmean0427 <youmean0427@gmail.com>
2023-10-19 23:44:47 +0900

    [feat] Create Model CoffeeCafe, Review

--- coffeecafes/admin.py
+++ coffeecafes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from coffeecafes.models import CoffeeCafe
+from coffeecafes.models import CoffeeCafe, Review
 
 
 admin.site.register(CoffeeCafe)
+admin.site.register(Review)
--- coffeecafes/models.py
+++ coffeecafes/models.py
@@ -3,6 +3,23 @@ from django.db import models
 # Create your models here.
 
 class CoffeeCafe(models.Model):
-
-    title = models.CharField(max_length=100)
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, default="")
+    address = models.CharField(max_length=100)
+    time = models.CharField(max_length=100)
     total_score = models.FloatField()
+    lat = models.FloatField()
+    lng = models.FloatField()
+    image = models.ImageField(upload_to='images/', null=True, blank = True)
+
+
+
+class Review(models.Model):
+    id = models.IntegerField(primary_key=True)
+    cafe = models.ForeignKey(CoffeeCafe, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=500)
+    date = models.DateField()
+    score = models.FloatField()
+    type = models.IntegerField()
+    image = models.ImageField()
