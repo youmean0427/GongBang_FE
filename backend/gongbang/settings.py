79582a5803f804896bdc70bc0fac0e7365c766c3
youmean0427 <youmean0427@gmail.com>
2023-12-17 00:48:27 +0900

    [feat] Auth, Detail API Integration

--- backend/gongbang/settings.py
+++ backend/gongbang/settings.py
@@ -174,3 +174,5 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
+
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000/", "http://localhost:3000/"]
