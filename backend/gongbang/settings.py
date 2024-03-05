d8e938fbca76b52fa245017b459e7c46f6a45189
youmean0427 <youmean0427@gmail.com>
2024-03-05 21:05:08 +0900

    [test] Heroku

--- backend/gongbang/settings.py
+++ backend/gongbang/settings.py
@@ -22,14 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 
+'django-insecure-d4tx0#*x!wfroyn+$ace-_yqd_dd0)4-0^bonejv93=**wjm3w'
+)
 
-# )
-SECRET_KEY = 'django-insecure-d4tx0#*x!wfroyn+$ace-_yqd_dd0)4-0^bonejv93=**wjm3w'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False  # Django Rest Framework
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
