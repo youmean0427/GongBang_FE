7dbed5ad8532982e9c6160f95beb809239b8d8cb
youmean0427 <youmean0427@gmail.com>
2024-03-12 00:54:55 +0900

    [git] new Repo

--- backend/coffeecafes/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from coffeecafes.models import CoffeeCafe, Review
-
-admin.site.register(CoffeeCafe)
-admin.site.register(Review)
--- backend/coffeecafes/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoffeecafesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coffeecafes'
