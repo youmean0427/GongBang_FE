8acdd027ffe6f90b9d7be2d02c03621b2b4e278d
youmean0427 <youmean0427@gmail.com>
2024-03-13 21:35:42 +0900

    [refac] Folder

--- frontend/gongbang/src/utils.js
+++
@@ -1,5 +0,0 @@
-export const getCookie = (name) => {
-    const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
-    return cookieValue ? cookieValue.pop() : null;
-  };
-  
